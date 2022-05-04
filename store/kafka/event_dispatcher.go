2ea687b8d8a1e5e26d504817742a0651b1d4b169
Trung Nguyen <trung.nguyen@sioux.asia>
2022-05-05 00:06:30 +0700

    kafka implements event dispatcher for the app

---
+++ store/kafka/event_dispatcher.go
@@ -0,0 +1,54 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"store/app/messaging"
+)
+
+type kafkaEvent struct {
+	event messaging.Event
+}
+
+func (e *kafkaEvent) Key() string {
+	return e.event.Key()
+}
+
+func (e *kafkaEvent) Topic() string {
+	return e.event.Type()
+}
+
+func (e *kafkaEvent) Value() []byte {
+	serialized, _ := json.Marshal(e.event)
+
+	return serialized
+}
+
+type eventDispatcher struct {
+	producers map[string]Producer
+}
+
+func (d *eventDispatcher) Dispatch(event messaging.Event) error {
+	if d.producers == nil {
+		d.producers = map[string]Producer{}
+	}
+
+	topic := event.Type()
+	producer, ok := d.producers[topic]
+	if !ok {
+		producer = NewProducer(topic)
+		d.producers[topic] = producer
+	}
+
+	return producer.Send(context.Background(), &kafkaEvent{event})
+}
+
+func (d *eventDispatcher) Dispose() {
+	if d.producers == nil {
+		return
+	}
+
+	for _, producer := range d.producers {
+		producer.Dispose()
+	}
+}
